4e4bf173b0f7bcb0f84ce3e5c479bf30632e2613
Jonathan Sitti <sitti.jonathan@gmail.com>
2022-04-20 18:28:09 +0200

    Updates

---
+++ precache-manifest.b7d2f25b95989bd98d278df1f41a7f05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85acd704b6ded23fd644de1574e5863a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb7834ac0a105daa3a3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0f5c204558d8b12c73c8",
+    "url": "/static/js/2.c1e22831.chunk.js"
+  },
+  {
+    "revision": "cfb7834ac0a105daa3a3",
+    "url": "/static/js/main.16a37783.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
