04982c8d8ac85ea4a11fee9e663f5438a65557f6
Jonathan Sitti <sitti.jonathan@gmail.com>
2022-04-20 18:11:21 +0200

    Create voting workflow

---
+++ client/src/App.js
@@ -0,0 +1,336 @@
+import React, { useState, useEffect, useRef } from "react";
+import VotingContract                         from "./contracts/Voting.json";
+import getWeb3                                from "./getWeb3";
+import "./App.css";
+
+const App = () => {
+
+  const [w3State, setState] = useState({
+    web3: null,
+    accounts: null,
+    contract: null,
+    owner: null
+  });
+
+  const [voterInput, setVoterInput] = useState("");
+  const [proposalInput, setProposalInput] = useState("");
+  const [currentUser, setCurrentUser] = useState({});
+  const [workflowStatus, setWorkFlowStatus] = useState("0");
+  const [proposals, setProposals] = useState([]);
+  const [proposalWinningId, setProposalWinningId] = useState(null);
+  const [votersCount, setVotersCount] = useState(0);
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3();
+  
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
+  
+        // Get the contract instance.
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = VotingContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          VotingContract.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
+        const owner = await instance.methods.owner().call();
+        const workflowStatus = await instance.methods.workflowStatus().call();
+          
+        setState({
+          web3,
+          accounts,
+          contract: instance,
+          owner
+        });
+        setWorkFlowStatus(workflowStatus);
+        
+        if (accounts[0] === owner) {
+          const currentUser = await instance.methods.getVoter(accounts[0]).call({ from: accounts[0] });
+          setCurrentUser(currentUser);
+          setIsRegistered(true)
+          const voters = await instance.methods.getVoters().call({ from: accounts[0] });
+          setVotersCount(voters.length);
+          const proposalsList = await instance.methods.getProposals().call({ from: accounts[0] });
+          setProposals(proposalsList);
+        } else {
+          let options = {
+            fromBlock: 0,
+            toBlock: 'latest'
+          };
+          const resetData = await instance.getPastEvents("ResetVote", options)
+      
+          options = {
+            // filter: {
+            //     value: "address"
+            // },
+            fromBlock: resetData[resetData.length - 1].returnValues.blockNumber,
+            toBlock: 'latest'
+          };  
+          const votersData = await instance.getPastEvents("VoterRegistered", options)
+          // console.log("votersData", votersData);
+          votersData.map(async event => {
+            if (event.returnValues.voterAddress === accounts[0]) {
+              setIsRegistered(true);
+              const currentUser = await instance.methods.getVoter(accounts[0]).call({ from: accounts[0] });
+              setCurrentUser(currentUser);
+
+              if (parseInt(workflowStatus) >= 1) {
+                const proposalsList = await instance.methods.getProposals().call({ from: accounts[0] });
+                setProposals(proposalsList);
+              }
+            }
+          });
+        }
+
+        await instance.events.VoterRegistered()
+          .on("data", async event => {
+              const voterAddress = event.returnValues.voterAddress;
+              if (voterAddress === accounts[0]) {
+                  const currentUser = await instance.methods.getVoter(accounts[0]).call({ from: accounts[0] });
+                  setCurrentUser(currentUser);
+                  setIsRegistered(true);
+              }
+              if (accounts[0] === owner) {
+                const voters = await instance.methods.getVoters().call({ from: accounts[0] });
+                setVotersCount(voters.length);
+              }
+              console.log("New voter have been registered: " + voterAddress);
+          })
+          .on("changed", changed => console.log(changed))
+          .on("error", err => console.error(err))
+          .on("connected", str => console.log(str));
+
+        await instance.events.ProposalRegistered()
+          .on("data", async event => {
+              const proposalsList = await instance.methods.getProposals().call({ from: accounts[0] });
+              setProposals(proposalsList);
+              console.log("New proposal pushed.");
+          })
+          .on("changed", changed => console.log(changed))
+          .on("error", err => console.error(err))
+          .on("connected", str => console.log(str));
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+        // if (state && state.contract && state.contract.methods) {
+          if (workflowStatus === "5") {
+              const { contract, owner } = w3State;
+              const proposalsList = await contract.methods.getProposals().call({ from: owner });
+              const proposalWinningId = await contract.methods.winningProposalID().call({ from: owner });
+              setProposals(proposalsList);
+              setProposalWinningId(proposalWinningId);
+          }
+    })()
+}, [workflowStatus]);
+
+  // useEffect(() => {
+  //   (function () {
+  //       if (workflowStatus === "0") {
+  //           setActiveStep(0);
+  //       } else if (
+  //           workflowStatus === "1" ||  
+  //           workflowStatus === "2"
+  //       ) {
+  //           setActiveStep(1);
+  //       } else if (
+  //           workflowStatus === "3" ||  
+  //           workflowStatus === "4"
+  //       ) {
+  //           setActiveStep(2);
+  //       } else if (workflowStatus === "5") {
+  //           setActiveStep(4);
+  //       }
+  //   })();
+  // }, [workflowStatus]);
+
+  console.log("currentUser2", currentUser);
+
+  const handleChangeVoter = (e) => {
+    e.preventDefault();
+    setVoterInput(e.target.value);
+  }
+
+  const handleChangeProposal = (e) => {
+    e.preventDefault();
+    setProposalInput(e.target.value);
+  }
+
+  const handleSubmitVoter = async () => {
+    const { accounts, contract, owner } = w3State;
+    await contract.methods.addVoter(voterInput).send({ from: owner });
+  }
+
+  const handleSubmitProposal = async () => {
+    const { accounts, contract, owner } = w3State;
+    await contract.methods.addProposal(proposalInput).send({ from: accounts[0] });
+  }
+
+  const syncWorkflowStatus = async () => {
+    const workflowStatus = await w3State.contract.methods.workflowStatus().call();
+    setWorkFlowStatus(workflowStatus);
+  }
+
+  const startProposalsRegistering = async () => {
+    const { accounts, contract, owner } = w3State;
+    await w3State.contract.methods.startProposalsRegistering().send({ from: owner });
+    await syncWorkflowStatus();
+  }
+
+  const endProposalsRegistering = async () => {
+    const { accounts, contract, owner } = w3State;
+    await contract.methods.endProposalsRegistering().send({ from: owner });
+    await syncWorkflowStatus();
+  }
+
+  const startVotingSession = async () => {
+    const { accounts, contract, owner } = w3State;
+    await contract.methods.startVotingSession().send({ from: owner });
+    await syncWorkflowStatus();
+  }
+
+  const endVotingSession = async () => {
+    const { accounts, contract, owner } = w3State;
+    await contract.methods.endVotingSession().send({ from: owner });
+    await syncWorkflowStatus();
+  }
+
+  const tallyVotes = async () => {
+    const { accounts, contract, owner } = w3State;
+    await contract.methods.tallyVotes().send({ from: owner });
+    await syncWorkflowStatus();
+  }
+
+  const resetVote = async () => {
+    const { accounts, contract, owner } = w3State;
+    await contract.methods.resetVote().send({ from: owner });
+    await syncWorkflowStatus();
+    setProposals([]);
+    const voters = await contract.methods.getVoters().call({ from: accounts[0] });
+    setVotersCount(voters.length);
+  }
+
+  const isOwner = () => w3State.accounts[0] === w3State.owner;
+
+  const renderLoggedUser = () => {
+    if (isOwner()) {
+      return "owner";
+    }
+
+    if (isRegistered) {
+      return "registered";
+    }
+
+    return "not registered";
+  };
+
+  const renderVotersCount = () => <p>{votersCount} registered {votersCount > 1 ? "voters" : "voter"}</p>
+
+  const renderStepButton = () => {
+    switch (workflowStatus) {
+      case "0":
+        return <button onClick={startProposalsRegistering}>Start proposal registration</button>;
+      case "1":
+        return <button onClick={endProposalsRegistering}>End proposal registration</button>;
+      case "2":
+        return <button onClick={startVotingSession}>Start voting session</button>;
+      case "3":
+        return <button onClick={endVotingSession}>End voting session</button>;
+      case "4":
+        return <button onClick={tallyVotes}>Tally votes</button>;
+      case "5":
+        return <button onClick={resetVote}>Reset vote</button>;
+    }
+  }
+
+  const vote = async index => {
+    const { accounts, contract } = w3State;
+    await contract.methods.setVote(index).send({ from: accounts[0] });
+    const currentUser = await contract.methods.getVoter(accounts[0]).call();
+    setCurrentUser(currentUser);
+  }
+
+  const renderVoterButton = () => {
+    return (
+      <div>
+        <br /><br />
+        <input value={voterInput} onChange={handleChangeVoter} />
+        <button onClick={handleSubmitVoter}>Add voter</button>
+      </div>
+    )
+  }
+
+  const wrapperProposal = () => {
+    return (
+      <div>
+        {workflowStatus === "1" && (
+          <>
+            <input value={proposalInput} onChange={handleChangeProposal} />
+            <button onClick={handleSubmitProposal}>Add proposal</button>
+          </>
+        )}
+        {workflowStatus !== "0" && renderProposals(proposals)}
+      </div>
+    )
+  }
+
+  const renderProposals = (proposals) => {
+    if (proposals.length === 0) {
+      return <p>There is no proposal registered yet.</p>
+    }
+  
+    return (
+      <div>
+        {proposalWinningId && <p><b>The winner proposal is: {proposals[proposalWinningId].description}</b></p>}
+        Proposals:
+        {proposals.map((proposal, index) =>
+          <div key={index}>
+            <p>{proposal.description}</p>
+            {workflowStatus === "3" && !currentUser.hasVoted &&
+            <button onClick={() => vote(index)}>Vote</button>}
+            {workflowStatus === "5" &&
+            <div>{proposal.voteCount} {proposal.voteCount > 1 ? "votes" : "vote"}</div>}
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  if (w3State.web3 === null) {
+    return <div>Loading Web3, accounts, and contract...</div>;
+  }
+
+  return (
+    <div className="App">
+      <h1>Voting dApp project</h1>
+      <p>Truffle react box example</p>
+      <p><strong>You are {renderLoggedUser()} ({w3State.accounts[0]})</strong></p>
+      {isOwner() &&
+      <>
+        <br />
+        <button onClick={resetVote}>Reset vote</button>
+        <br />
+      </>}
+      {isOwner() && renderVotersCount(votersCount)}
+      {isOwner() && renderStepButton()}
+      {isOwner() && workflowStatus === "0" && renderVoterButton()}
+      {isRegistered && wrapperProposal()}
+    </div>
+  );
+}
+
+export default App;
---
+++ contracts/Voting.sol
@@ -0,0 +1,286 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Voting smart contract
+ *
+ * @author Jonacity
+ *
+ * @dev A simple voting system
+ */
+contract Voting is Ownable {
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    uint8 public winningProposalID;
+    uint8 maxProposals = 50;
+    Proposal[] proposalsArray;
+    address[] votersArray;
+    mapping (address => Voter) voters;
+    WorkflowStatus public workflowStatus;
+
+    event VoterRegistered(address voterAddress);
+    event ProposalRegistered(uint proposalId);
+    event Voted (address voter, uint proposalId);
+    event ResetVote(uint date, uint blockNumber);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+
+    /**
+     * @dev Constructor: the contract owner is added as voter
+     */
+    constructor() {
+        addVoter(msg.sender);
+    }
+
+    /**
+     * @dev Check if the sender is a registered voter
+     */
+    modifier onlyVoters() {
+        require(voters[msg.sender].isRegistered, "You're not a voter");
+        _;
+    }
+
+    // ::::::::::::: GETTERS ::::::::::::: //
+    /**
+     * @dev Get a voter from an address
+     *
+     * @param _addr Address of the user
+     *
+     * @return Voter Voter details
+     */
+    function getVoter(address _addr) external onlyVoters view returns (Voter memory) {
+        return voters[_addr];
+    }
+
+    /**
+     * @dev Get a proposal from an id
+     *
+     * @param _id Proposal id
+     *
+     * @return Proposal Proposal details
+     */
+    function getOneProposal(uint _id) external onlyVoters view returns (Proposal memory) {
+        return proposalsArray[_id];
+    }
+
+    /**
+     * @dev Get all proposals
+     *
+     * @return Proposal[] Added proposals list
+     */
+    function getProposals() external onlyVoters view returns (Proposal[] memory) {
+        return proposalsArray;
+    }
+
+    /**
+     * @dev Get all voters addresses
+     *
+     * @return address[] Registered voters addresses
+     */
+    function getVoters() external onlyVoters view returns (address[] memory) {
+        return votersArray;
+    }
+
+    // ::::::::::::: REGISTRATION ::::::::::::: //
+    /**
+     * @dev Register a voter and add his address into the votersArray
+     *
+     * @param _addr Voter address
+     */
+    function addVoter(address _addr) public onlyOwner {
+        require(workflowStatus == WorkflowStatus.RegisteringVoters, 'Voters registration is not open yet');
+        require(voters[_addr].isRegistered != true, 'Already registered');
+
+        voters[_addr].isRegistered = true;
+        votersArray.push(_addr);
+        emit VoterRegistered(_addr);
+    }
+
+    /* facultatif
+     * function deleteVoter(address _addr) external onlyOwner {
+     *   require(workflowStatus == WorkflowStatus.RegisteringVoters, 'Voters registration is not open yet');
+     *   require(voters[_addr].isRegistered == true, 'Not registered.');
+     *   voters[_addr].isRegistered = false;
+     *  emit VoterRegistered(_addr);
+    }*/
+
+    // ::::::::::::: PROPOSAL ::::::::::::: //
+    /**
+     * @dev Create a proposal
+     *
+     * @param _desc Proposal description
+     */
+    function addProposal(string memory _desc) external onlyVoters {
+        require(workflowStatus == WorkflowStatus.ProposalsRegistrationStarted, 'Proposals are not allowed yet');
+        require(keccak256(abi.encode(_desc)) != keccak256(abi.encode("")), 'Vous ne pouvez pas ne rien proposer'); // facultatif
+        require(proposalsArray.length <= maxProposals, "For this vote, you are allowed to create 50 proposals max");
+        // voir que desc est different des autres
+
+        Proposal memory proposal;
+        proposal.description = _desc;
+        proposalsArray.push(proposal);
+        emit ProposalRegistered(proposalsArray.length-1);
+    }
+
+    // ::::::::::::: VOTE ::::::::::::: //
+    /**
+     * @dev Vote to a proposal
+     *
+     * @param _id Proposal id
+     */
+    function setVote( uint _id) external onlyVoters {
+        require(workflowStatus == WorkflowStatus.VotingSessionStarted, 'Voting session havent started yet');
+        require(voters[msg.sender].hasVoted != true, 'You have already voted');
+        require(_id <= proposalsArray.length, 'Proposal not found'); // pas obligé, et pas besoin du >0 car uint
+
+        voters[msg.sender].votedProposalId = _id;
+        voters[msg.sender].hasVoted = true;
+        proposalsArray[_id].voteCount++;
+        emit Voted(msg.sender, _id);
+    }
+
+    // ::::::::::::: STATE ::::::::::::: //
+    /* on pourrait factoriser tout ça: par exemple:
+    *
+    *  modifier checkWorkflowStatus(uint  _num) {
+    *    require (workflowStatus=WorkflowStatus(uint(_num)-1, "bad workflowstatus");
+    *    require (num != 5, "il faut lancer tally votes");
+    *    _;
+    *  }
+    *
+    *  function setWorkflowStatus(WorkflowStatus _num) public checkWorkflowStatus( _num) onlyOwner {
+    *    WorkflowStatus old = workflowStatus;
+    *    workflowStatus = WorkflowStatus(_num);
+    *    emit WorkflowStatusChange(old, workflowStatus);
+    *   }
+    *
+    *  ou plus simplement:
+    *  function nextWorkflowStatus() onlyOwner{
+    *    require (uint(workflowStatus)!=4, "il faut lancer tallyvotes");
+    *    WorkflowStatus old = workflowStatus;
+    *    workflowStatus= WorkflowStatus(uint (workflowstatus) + 1);
+    *    emit WorkflowStatusChange(old, workflowStatus);
+    *  }
+    *
+    */
+
+    /**
+     * @dev Start the proposals registering
+     */
+    function startProposalsRegistering() external onlyOwner {
+        require(workflowStatus == WorkflowStatus.RegisteringVoters, 'Registering proposals cant be started now');
+
+        workflowStatus = WorkflowStatus.ProposalsRegistrationStarted;
+        emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationStarted);
+    }
+
+    /**
+     * @dev End the proposals registering
+     */
+    function endProposalsRegistering() external onlyOwner {
+        require(workflowStatus == WorkflowStatus.ProposalsRegistrationStarted, 'Registering proposals havent started yet');
+
+        workflowStatus = WorkflowStatus.ProposalsRegistrationEnded;
+        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted, WorkflowStatus.ProposalsRegistrationEnded);
+    }
+
+    /**
+     * @dev Start the voting session
+     */
+    function startVotingSession() external onlyOwner {
+        require(workflowStatus == WorkflowStatus.ProposalsRegistrationEnded, 'Registering proposals phase is not finished');
+
+        workflowStatus = WorkflowStatus.VotingSessionStarted;
+        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationEnded, WorkflowStatus.VotingSessionStarted);
+    }
+
+    /**
+     * @dev End the voting session
+     */
+    function endVotingSession() external onlyOwner {
+        require(workflowStatus == WorkflowStatus.VotingSessionStarted, 'Voting session havent started yet');
+
+        workflowStatus = WorkflowStatus.VotingSessionEnded;
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded);
+    }
+
+    /* function tallyVotesDraw() external onlyOwner {
+       require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");
+        uint highestCount;
+        uint[5]memory winners; // egalite entre 5 personnes max
+        uint nbWinners;
+        for (uint i = 0; i < proposalsArray.length; i++) {
+            if (proposalsArray[i].voteCount == highestCount) {
+                winners[nbWinners]=i;
+                nbWinners++;
+            }
+            if (proposalsArray[i].voteCount > highestCount) {
+                delete winners;
+                winners[0]= i;
+                highestCount = proposalsArray[i].voteCount;
+                nbWinners=1;
+            }
+        }
+        for(uint j=0;j<nbWinners;j++){
+            winningProposalsID.push(winners[j]);
+            winningProposals.push(proposalsArray[winners[j]]);
+        }
+        workflowStatus = WorkflowStatus.VotesTallied;
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
+    } */
+
+
+    /**
+     * @dev Tally the votes and set the proposal winner (winningProposalId)
+     */
+    function tallyVotes() external onlyOwner {
+            require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");
+
+            uint8 _winningProposalId;
+            for (uint8 p = 0; p < proposalsArray.length; p++) {
+                if (proposalsArray[p].voteCount > proposalsArray[_winningProposalId].voteCount) {
+                    _winningProposalId = p;
+                }
+            }
+            winningProposalID = _winningProposalId;
+            workflowStatus = WorkflowStatus.VotesTallied;
+            emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
+        }
+
+    /**
+     * @dev Reset the voting system restoring all variables to initial values
+     */
+    function resetVote() public onlyOwner {
+        // require(workflowStatus == WorkflowStatus.VotesTallied, "Votes not tallied");
+
+        for (uint n = 0; n < votersArray.length; n ++) {
+            delete voters[votersArray[n]];
+        }
+        delete votersArray;
+        delete proposalsArray;
+        winningProposalID = 0;
+        workflowStatus = WorkflowStatus.RegisteringVoters;
+        addVoter(msg.sender);
+        emit ResetVote(block.timestamp, block.number);
+    }
+
+}
